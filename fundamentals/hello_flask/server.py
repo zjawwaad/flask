8c9e8a3a16085f1fc38c2949bcceaed90b58e59c
zjawwaad <zjawwaad@yahoo.com>
2022-10-02 23:38:15 -0400

    html table commit

--- fundamentals/hello_flask/server.py
+++ fundamentals/hello_flask/server.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)  
 
-@app.route('/')          
+@app.route('/')
 def hello_world():
-    return render_template('index.html') 
+    return render_template('index.html', phrase="hello", times=5)
 
 @app.route('/success')
 def success():
@@ -22,6 +22,18 @@ def say(insertword):
 def repeat(insertnumber, insertword):
     print(insertnumber, insertword)
     return insertword * insertnumber
+
+@app.route('/lists')
+def render_lists():
+    student_info = [
+    {'name' : 'Michael', 'age' : 35},
+    {'name' : 'John', 'age' : 30 },
+    {'name' : 'Mark', 'age' : 25},
+    {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template('lists.html', random_numbers = [3,1,5], students = student_info)
+
+
 
 if __name__=="__main__":   
     app.run(debug=True)    
